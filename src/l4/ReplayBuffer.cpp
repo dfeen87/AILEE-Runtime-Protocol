c4494ed671eb0b861eb4ec3bb0fd4140b93c5997
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-07 15:35:56 -0400

    Create ReplayBuffer.cpp

---
+++ src/l4/ReplayBuffer.cpp
@@ -0,0 +1,33 @@
+#include "l4/ReplayBuffer.h"
+
+namespace ailee {
+namespace l4 {
+
+void ReplayBuffer::record_tick(
+    const DeterministicSchedulerState& scheduler_state,
+    const ClusterView& view,
+    const TelemetrySample& telemetry_sample
+) {
+    scheduler_snapshots.push_back({ scheduler_state });
+    
+    ReplayClusterViewSnapshot snap;
+    
+    // Perform true deep copies of contents, not pointers
+    snap.nodes.reserve(view.nodes.size());
+    for (const auto& node : view.nodes) {
+        snap.nodes.push_back(node);
+    }
+    
+    snap.mesh_envelopes = view.mesh_envelopes;
+    snap.transport_queue = view.transport_queue;
+    snap.total_nodes = view.total_nodes;
+    snap.total_steps = view.total_steps;
+    snap.coherence_summary = view.coherence_summary;
+    
+    view_snapshots.push_back(std::move(snap));
+    
+    telemetry_snapshots.push_back(telemetry_sample);
+}
+
+} // namespace l4
+} // namespace ailee
