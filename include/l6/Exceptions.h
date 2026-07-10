85cfe0659611eccd54c42b20dcb2b33dcca4bc2e
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-10 16:12:26 -0400

    Create Exceptions.h

---
+++ include/l6/Exceptions.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace ailee::l6 {
+
+class DeterministicBackendException : public std::runtime_error {
+public:
+    explicit DeterministicBackendException(const std::string& msg)
+        : std::runtime_error(msg) {}
+};
+
+} // namespace ailee::l6
