7a0d2dc72c8baee36541e1726d3980f20c9aba87
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-07-14 10:55:10 -0400

    Update AmbientProtocol.cpp

--- src/protocol/AmbientProtocol.cpp
+++ src/protocol/AmbientProtocol.cpp
@@ -1,6 +1,7 @@
 #include "AmbientProtocol.hpp"
 #include "ProtocolFrame.hpp"
 #include <iostream>
+#include <ctime>
 
 // ---------------------------------------------------------
 // Process inbound protocol frame
@@ -27,6 +28,8 @@ Json::Value AmbientProtocol::buildProtocolResponse(const std::string& type)
     Json::Value response;
     response["response_type"] = type;
     response["status"] = "ok";
+
+    // Correct timestamp syntax
     response["timestamp"] = (Json::UInt64) std::time(nullptr);
 
     return response;
